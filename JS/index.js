36a25f361fbcd2b6ed89b47622ffb9882ca31704
shin-102 <hydro.wild@gmail.com>
2024-02-06 18:29:36 +0100

    init git + push request

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+//*NAVIGATION HEADER SECTION
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button[aria-controls='mobile-menu']");
+  const menu = document.getElementById("mobile-menu");
+
+  button.addEventListener("click", function () {
+  const expanded = this.getAttribute("aria-expanded") === "true" || false;
+
+  this.setAttribute("aria-expanded", !expanded);
+  menu.classList.toggle("hidden");
+  });
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('ani'); //ani is the "show" class
+    } else {
+      entry.target.classList.remove('ani');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.unani'); //unani is the "hidden" class
+hiddenElements.forEach((el) => observer.observe(el));
+
+//*ABOUT SECTION CAROUSEL
+
+
+
